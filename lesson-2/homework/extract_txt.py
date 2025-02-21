a0293136d6b15e79f41dede42a82a907326d198e
Abdurakhmon14 <abdurahmon.bek14@gmail.com>
2025-02-22 00:44:47 +0500

    Update extract_txt.py

--- lesson-2/homework/extract_txt.py
+++ lesson-2/homework/extract_txt.py
@@ -1,1 +1,17 @@
 
+##Homework1
+##extract car names from this text
+
+txt='MsaatmiazD'
+car1= txt[::2]
+
+
+car2 = txt[-1::-2]
+
+print(F"Name of the first car is {car1} \n Second car name is {car2}")
+
+##Homework2 
+
+txt = "I' am John. I am from London"
+r = txt.split()
+r[-1]
