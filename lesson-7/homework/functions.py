63081b86e2670c07587b439269e6027fc735e262
Abdurakhmon14 <abdurahmon.bek14@gmail.com>
2025-02-22 01:23:39 +0500

    Create functions.py

---
+++ lesson-7/homework/functions.py
@@ -0,0 +1,22 @@
+# Homework 
+#Count Vowels in astring
+#problem: count the number of vowels (a,e,i,o,u) in a given string
+
+text = "hello world"
+count = 0
+for char in text:
+    if char in "aeuioAEUIO":
+        count += 1
+print(f'Vowels in {text}: {count}')
+
+MAXIMUM NUMBER  IN A LIST  
+find the maximum number in a list using a loop (without using max()).
+
+ls = [1,6,83,6,2, -100]
+max_num = 0
+for num in ls:
+    if num>max_num:
+        max_num = num
+print(f'Max number in a list: {max_num}')
+
+
